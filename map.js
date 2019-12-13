d2c3db0521836e9d6a5ac5a20faa033a269062a4
lee03366 <55416671+lee03366@users.noreply.github.com>
2019-12-12 18:50:00 -0600

    Add files via upload

--- map.js
+++ map.js
@@ -1,4 +1,6 @@
 function Init(){
+
+
 	
 	//Change to port that is input on page load
 	var port = 8000;
@@ -59,12 +61,12 @@ function Init(){
 						var temp = this.message.split(",");
 						if(!isNaN(temp[0])) {
 							var coordinates = L.latLng(parseFloat(temp[0]), parseFloat(temp[1]));
-							map.setView(coordinates);		
+							map.setView(coordinates, 18);		
 							
 						} else {
 							var xhttp = new XMLHttpRequest();
 							console.log(temp[0]);
-							var url= 'https://nominatim.openstreetmap.org/search?street='+temp[0]+"&format=json";
+							var url= 'https://nominatim.openstreetmap.org/search?street='+temp[0]+"&city=St Paul&format=json";
 							xhttp.open("GET",url);
 							xhttp.send();
 							xhttp.onreadystatechange = function() {
@@ -92,18 +94,26 @@ function Init(){
 					}
 				}
 			});	
-			
-			
+
 			var map = L.map('map').setView([44.938500,-93.094225], 12);
 			map.setMaxBounds([[44.892384,-93.206011],[44.991944,-93.005194]]);
+		
+			L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+				attribution: '© <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+				maxZoom: 18,
+				minZoom: 11,
+				id: 'mapbox/streets-v11',
+				accessToken: 'pk.eyJ1IjoibGVlMDMzNjYiLCJhIjoiY2szemJ5ZHB2MW4wNjNudDZ6NmozNjI5byJ9.031whUscC0xfmxgrOKEOQA'
+			}).addTo(map);
+
 			
+
 			/* This was just testing the leaflet functionality to help me understand it
 			var bounds = map.getBounds();
 			console.log("MaxBounds: " + bounds.getNorthEast());
 			console.log("MinBounds: " + bounds.getSouthWest());
 			app.data.lat_long_ne=bounds.getNorthEast();
 			console.log("XXXXX: " + data.lat_long_ne);
-
 			map.on('zoomend', function() {
 				
 				bounds = map.getBounds();
@@ -125,13 +135,7 @@ function Init(){
 			});
 			*/
 			
-			L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-				attribution: '© <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-				maxZoom: 18,
-				minZoom: 11,
-				id: 'mapbox/streets-v11',
-				accessToken: 'pk.eyJ1IjoibGVlMDMzNjYiLCJhIjoiY2szemJ5ZHB2MW4wNjNudDZ6NmozNjI5byJ9.031whUscC0xfmxgrOKEOQA'
-			}).addTo(map);
+
 		}
 	}
 }
